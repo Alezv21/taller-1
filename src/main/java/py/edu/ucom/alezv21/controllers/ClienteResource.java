73e87cdf0150872498ce203afcffa761409be359
Alejandro Zavala <alezv21@hotmail.com>
2023-10-25 07:03:01 -0300

    prueba

---
+++ src/main/java/py/edu/ucom/alezv21/controllers/ClienteResource.java
@@ -0,0 +1,43 @@
+package py.edu.ucom.alezv21.controllers;
+
+import java.util.List;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import py.edu.ucom.alezv21.services.ClienteService;
+import py.edu.ucom.alezv21.entities.Cliente;
+
+@Path("/cliente")
+public class ClienteResource {
+    
+    @Inject
+    public ClienteService service;
+
+    @GET
+    public List<Cliente> listar(){
+        return this.service.listar();
+    }
+    @DELETE
+    @Path("{id}")
+    public void eliminar(Integer id) {
+        this.service.eliminar(id);
+    }
+    @POST
+    public Cliente agregar (Cliente param){
+        return this.service.agregar(param);
+    }
+    @PUT
+    public Cliente modificar (Cliente param){
+        return this.service.modificar(param);
+    }
+    @GET
+    @Path("{id}")
+    public Cliente obtener(@PathParam("id")Integer param){
+        return this.service.obtener(param);     
+    }
+}
